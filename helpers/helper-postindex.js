d63dfba5001bb1a0062a4c438d9a5c4db3e00850
Andrew Jones <ascjones@gmail.com>
2013-09-19 16:09:50 +0200

    show whole of first post and excerpts of other posts using helper

---
+++ helpers/helper-postindex.js
@@ -0,0 +1,39 @@
+(function() {
+  module.exports.register = function(Handlebars, options) {
+
+    var grunt = require('grunt');
+
+    /**
+    * TODO: add comment
+    */
+    Handlebars.registerHelper("postindex", function (pages, count, options) {
+      // only show posts that were posted today or in the past
+      var posts = pages
+          .filter(function(page) {
+            return page.src.indexOf('posts') > -1 && page.data.date < new Date();
+          })
+          .sort(function(p1, p2) {
+            var date1 = p1.data.date;
+            var date2 = p2.data.date;
+            if (date1 === date2) {
+              return 0;
+            }
+            return date1 < date2 ? -1 : 1;
+          })
+          .slice(0, count);
+
+      var i;
+      var len;
+      var result = '';
+      var index;
+      for (index = i = 0, len = posts.length; i < len; index = ++i) {
+        var post = posts[index];
+        result += options.fn({
+          post: post,
+          isFirst: index === 0
+        });
+      }
+      return result;
+    });
+  };
+}).call(this);
